a6fd50f8fb928a401469f1c218545c279c287dd7
Zohaib Rashid <47440269+ZohaibRashid@users.noreply.github.com>
2022-05-28 23:39:26 -0400

    create avatar component avatar component contains the image's url so it can be utilized elsewhere in the project if needed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Avatar from "./Avatar";
+import Details from "./Details";
 
 function Card(props) {
   return (
@@ -6,11 +8,11 @@ function Card(props) {
       <div className="card">
         <div className="top">
           <h2 className="name">{props.name}</h2>
-          <img className="circle-img" src={props.imgURL} alt="avatar_img" />
+          <Avatar imgURL={props.imgURL} />
         </div>
         <div className="bottom">
-          <p className="info">{props.phone}</p>
-          <p className="info">{props.email}</p>
+          <Details contactDetail={props.phone} />
+          <Details contactDetail={props.email} />
         </div>
       </div>
     </div>
---
+++ src/components/Details.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Details(props) {
+  return <p className="info">{props.contactDetail}</p>;
+}
+export default Details;
